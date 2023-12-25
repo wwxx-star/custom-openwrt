fc4cfb2c8896f47851326df16b41b5ebbd83f9e5
Thanh Phat <baonq@live.com>
2023-12-25 09:03:11 +0700

    disable batman adv feed

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,7 +16,7 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git batman https://git.open-mesh.org/openwrt-feed-devel.git' >>feeds.conf.default
+#echo 'src-git batman https://git.open-mesh.org/openwrt-feed-devel.git' >>feeds.conf.default
 echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
 echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >>feeds.conf.default
 echo 'src-git passwall_2 https://github.com/xiaorouji/openwrt-passwall2.git;main' >>feeds.conf.default
