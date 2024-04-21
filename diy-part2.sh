3fa9e030dea9f5a936553ecba95e5c97fbc5efae
wwxx-star <58853590+wwxx-star@users.noreply.github.com>
2024-04-21 23:12:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,10 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
+git clone https://github.com/xiaoxiao29/luci-app-adguardhome.git  package/luci-app-adguardhome
+./scripts/feeds update -a && ./scripts/feeds install -a
+
+
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
+
